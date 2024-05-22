f311807d08075781634ff17621ec007dce6391e8
Scott Feichter <scottfeichter@gmail.com>
2024-05-22 13:21:51 -0700

    almost

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,48 @@
 export function getAllDogs() {
-    // Your code here 
+    // Your code here
+    return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here 
+    // Your code here
+    return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here 
+    // Your code here
+    return fetch("/dogs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            name: "little bear",
+            age: 14
+        })
+    })
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here 
+     // Your code here
+     return fetch("/dogs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            name: "big bear",
+            age: 7
+        })
+    })
 }
 
 export function deleteDog(id) {
-      // Your code here 
+      // Your code here
+
+      return fetch(`/dogs/${id}`, {
+        method: "POST",
+        headers: {
+            AUTH:"ckyut5wau0000jyv5bsrud90y"
+        }
+      })
 }
